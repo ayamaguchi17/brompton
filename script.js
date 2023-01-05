5fad89793f8c2b714a6bd63348d9ba3743ae6cbe
ayamaguchi17 <111441372+ayamaguchi17@users.noreply.github.com>
2023-01-05 22:19:53 +0900

    Font update

--- script.js
+++ script.js
@@ -42,14 +42,14 @@ let data = {
     aboutTitle: "メイドインロンドン",
     aboutName: "1975年創設",
     aboutText:
-      "BROMPTON（ブロンプトン）は、1975年に発明された折りたたみ自転車で、２０秒以内にホイールよりも少し大きいロック式に容易に折りたためるよう巧みに設計されています。他に類をみない折りたたみ自転車です。1975年、ブロンプトンオラトリーを見下ろすアパートの寝室で、彼は独創的な3つの部分からなる折り畳み式自転車を発明しました。小さなパッケージに変身し、彼と一緒にどこにでも行ける乗り物です。街の「魔法のじゅうたん」。2005年に発売されたBromptonスーパーライトモデルは、軽量のチタン製リアフレームとフォークで設計されており、自転車と企業の両方にとっての進化を表しています。その後まもなく、プロジェクトのリードエンジニアの1人であるウィル・バトラーアダムスが最高経営責任者の役割を引き継ぎました。現在、テクニカルディレクターとして、アンドリューはエンジニアリングをもっぱら担当しています。2016年に辞任する前に、彼の元の特許で概説されている未来のアイデア、つまり「電気モーター」を備えたBrompton 自転車が実現しました",
+      "BROMPTON（ブロンプトン）は、1975年に発明された折りたたみ自転車で、２０秒以内にホイールよりも少し大きいロック式に容易に折りたためるよう巧みに設計されています。他に類をみない折りたたみ自転車です。1975年、ブロンプトンオラトリーを見下ろすアパートの寝室で、彼は独創的な3つの部分からなる折り畳み式自転車を発明しました。小さなパッケージに変身し、彼と一緒にどこにでも行ける乗り物です。街の「魔法のじゅうたん」。2005年に発売されたBROMPTONスーパーライトモデルは、軽量のチタン製リアフレームとフォークで設計されており、自転車と企業の両方にとっての進化を表しています。その後まもなく、プロジェクトのリードエンジニアの1人であるウィル・バトラーアダムスが最高経営責任者の役割を引き継ぎました。現在、テクニカルディレクターとして、アンドリューはエンジニアリングをもっぱら担当しています。2016年に辞任する前に、彼の元の特許で概説されている未来のアイデア、つまり「電気モーター」を備えたBROMPTON自転車が実現しました",
     bikeTitle: "折りたたみ自転車",
     bikeName1: "愛坊",
     bikeText1:
-      "長さ585mm、高さ565mm、奥行き270mm。しっかり固定された状態の小型でコンパクトなパッケージに変形するように巧みに設計されています。汚れた部分はいずれも中に封じ込められています。折りたたまれたBromptonは、けん引、収納、滑走そして転がすことができます。",
+      "長さ585mm、高さ565mm、奥行き270mm。しっかり固定された状態の小型でコンパクトなパッケージに変形するように巧みに設計されています。汚れた部分はいずれも中に封じ込められています。折りたたまれたBROMPTONは、けん引、収納、滑走そして転がすことができます。",
     bikeName2: "自由",
     bikeText2:
-      "Bromptonは折りたたまれて小さなコンパクトなパッケージになり、全体がしっかり固定された状態になります。20秒未満で。Brompton自転車はラッシュアワーの電車でも大歓迎です-予約する必要はありません。小型車のトランクの中。タクシー、ボート、フェリー、路面電車、バス。自転車を会議、カフェ、図書館、スーパーマーケットに持って行ってください。機能的な設計はあなたの調子に適応し、あなたと一緒にどこでも移動します。",
+      "BROMPTONは折りたたまれて小さなコンパクトなパッケージになり、全体がしっかり固定された状態になります。20秒未満で。BROMPTON自転車はラッシュアワーの電車でも大歓迎です-予約する必要はありません。小型車のトランクの中。タクシー、ボート、フェリー、路面電車、バス。自転車を会議、カフェ、図書館、スーパーマーケットに持って行ってください。機能的な設計はあなたの調子に適応し、あなたと一緒にどこでも移動します。",
     bikeName3: "高性能",
     bikeText3:
       "BROMPTON（ブロンプトン）バイクは製造工程が他のバイクとは異なります。弊社のバイクは、街中やその先へ移動するために適し、効率的で快適なライドが楽しめるよう設計されています。 機能性だけでなく、それ以上のパフォーマンスを発揮するバイクであり、今までに体験したことのない「BROMPTONを手にするまで得られない、BROMPTONだけのバイクライド」をお楽しみいただけます。",
@@ -60,14 +60,14 @@ let data = {
     aboutTitle: "Made in London",
     aboutName: "Since 1975",
     aboutText:
-      "BROMPTON is a folding bicycle invented in 1975, cleverly designed to fold easily into a locking mechanism slightly larger than the wheels in less than 20 seconds. It is a folding bicycle like no other: in 1975, in the bedroom of his apartment overlooking the Brompton Oratory, he invented an ingenious three-part folding bicycle. It transforms into a small package, a vehicle that can go anywhere with him. The city's magic carpet, the Brompton Superlight model launched in 2005, designed with a lightweight titanium rear frame and fork, represented an evolution for both the bike and the company. Shortly thereafter, Will Butler-Adams, one of the lead engineers on the project, took over the role of CEO. Now, as technical director, Andrew is solely responsible for engineering, and before he resigned in 2016, the future idea outlined in his original patent - a Brompton with an electric motor bicycle became a reality.",
+      "BROMPTON is a folding bicycle invented in 1975, cleverly designed to fold easily into a locking mechanism slightly larger than the wheels in less than 20 seconds. It is a folding bicycle like no other: in 1975, in the bedroom of his apartment overlooking the BROMPTON Oratory, he invented an ingenious three-part folding bicycle. It transforms into a small package, a vehicle that can go anywhere with him. The city's magic carpet, the BROMPTON Superlight model launched in 2005, designed with a lightweight titanium rear frame and fork, represented an evolution for both the bike and the company. Shortly thereafter, Will Butler-Adams, one of the lead engineers on the project, took over the role of CEO. Now, as technical director, Andrew is solely responsible for engineering, and before he resigned in 2016, the future idea outlined in his original patent - a BROMPTON with an electric motor bicycle became a reality.",
     bikeTitle: "Folding Bike",
     bikeName1: "Companionship",
     bikeText1:
-      "585 mm length, 565 mm tall, 270 mm depth, cleverly designed to transform into a small, compact package that always stays firmly in place. Grease or dirt can be prevented as they are sealed within the fold. The folded Brompton can be towed, stowed, glided and rolled!",
+      "585 mm length, 565 mm tall, 270 mm depth, cleverly designed to transform into a small, compact package that always stays firmly in place. Grease or dirt can be prevented as they are sealed within the fold. The folded BROMPTON can be towed, stowed, glided and rolled!",
     bikeName2: "Freedom",
     bikeText2:
-      "Experience freedom  traveling with our Brompton bicycles. You can store them in the trunk of a small car, cabs, ferries, trams, buses, you name it. Take your bike to meetings, cafes, libraries, or to the groceries. The functional design adapts to your tune and travels everywhere with you.",
+      "Experience freedom  traveling with our BROMPTON bicycles. You can store them in the trunk of a small car, cabs, ferries, trams, buses, you name it. Take your bike to meetings, cafes, libraries, or to the groceries. The functional design adapts to your tune and travels everywhere with you.",
     bikeName3: "High Performance",
     bikeText3:
       "Our bikes are designed to be suitable for moving around town and beyond, and to enjoy an efficient and comfortable ride. They are bikes that perform above and beyond their functionality, and offer you a BROMPTON-only bike ride like you've never experienced before, one that you won't get until you get your hands on a BROMPTON!",
