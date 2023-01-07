6646cd8bffa36f70974fb127e8defed4c0c2d8f1
ayamaguchi17 <111441372+ayamaguchi17@users.noreply.github.com>
2023-01-07 20:37:18 +0900

    Scroll btn

--- script.js
+++ script.js
@@ -73,3 +73,27 @@ let data = {
       "Our bikes are designed to be suitable for moving around town and beyond, and to enjoy an efficient and comfortable ride. They are bikes that perform above and beyond their functionality, and offer you a BROMPTON-only bike ride like you've never experienced before, one that you won't get until you get your hands on a BROMPTON!",
   },
 }
+
+
+// scroll up
+
+let upBtn = document.getElementById("up-btn")
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      upBtn.style.display = "block";
+    } else {
+      upBtn.style.display = "none";
+    }
+  }
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollUp() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
